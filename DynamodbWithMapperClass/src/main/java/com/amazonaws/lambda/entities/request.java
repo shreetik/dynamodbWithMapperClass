1c37ee6a72a121548caf8fc68351cbe66b26289e
ShreetikSahani <ShreetikSahani@LAPTOP-TPJ3H7TR>
2022-04-12 22:48:43 +0530

    first commit

---
+++ DynamodbWithMapperClass/src/main/java/com/amazonaws/lambda/entities/request.java
@@ -0,0 +1,75 @@
+package com.amazonaws.lambda.entities;
+
+public class request {
+
+	private String action;
+	private String status;
+	private String msg;
+	private String empId;
+	private String empName;
+	private String designation;
+	private String email;
+
+	public String getAction() {
+		return action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "request [action=" + action + ", status=" + status + ", msg=" + msg + ", empId=" + empId + ", empName="
+				+ empName + ", designation=" + designation + ", email=" + email + "]";
+	}
+
+}
